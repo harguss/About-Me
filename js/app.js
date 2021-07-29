b7e71747a2b40dcd33f441d0ef5e0408df1f8b89
Ian Whitmore <ianwhitmor@gmail.com>
2021-07-29 14:07:30 -0500

    correct js errors

--- js/app.js
+++ js/app.js
@@ -2,116 +2,92 @@
 console.log('js file connected');
 
 
-// //prompt our user for input data
-// let userName = prompt('What is your name?').toLowerCase();
-// console.log('user input is ', userName);
+//prompt our user for input data
+let userName = prompt('What is your name?').toLowerCase();
+console.log('user input is ', userName);
 
-// //greet the user
-// alert('Hello ' + userName);
-
-
-// let hourNow = prompt('What time is it? Respond in military time.');
-// console.log('the user time given ', hourNow);
-
-// if (hourNow >= 12 && hourNow >= 6){
-//   alert('Good Morning!');
-// } else if(hourNow >=12 && hourNow <= 18){
-//   alert('Good Afternoon!');
-// }else if (hourNow >= 3 && hourNow <=4){
-//   alert('Why are you up at this hour??? Go to Bed!');
-// } else {
-//   alert('Do you know what time it is?');
-// }
+//greet the user
+alert('Hello ' + userName);
 
 
+let hourNow = prompt('What time is it? Respond in military time.');
+console.log('the user time given ', hourNow);
+
+if (hourNow >= 12 && hourNow >= 6){
+  alert('Good Morning!');
+} else if(hourNow >=12 && hourNow <= 18){
+  alert('Good Afternoon!');
+}else if (hourNow >= 3 && hourNow <=4){
+  alert('Why are you up at this hour??? Go to Bed!');
+} else {
+  alert('Do you know what time it is?');
+}
 
 
 
 
-//3rd question
-// let favColor = prompt('what is your favorite color?');
-// console.log('user favColor is ', favColor);
+
+
+// 3rd question
+let favColor = prompt('what is your favorite color?');
+console.log('user favColor is ', favColor);
+
+alert(userName + ' your favorite color is ' + favColor);
+
+//4rd question
+
+let favDessert = prompt('What is your favorite dessert?');
+console.log('userName + is', favDessert);
+
+alert(userName + ' your favorite dessert is ' + favDessert);
+
+
+//5rd question
+let favAnimal = prompt('What is your favorite animal?');
+console.log('userName +  is', favAnimal);
 //respond
-//alert(userName + ' your favorite color is ' + favColor);
-
-// //4rd question
-
-//  let favDessert = prompt('What is your favorite dessert?');
-//  console.log('userName + is', favDessert);
-//respond
-//alert(userName + ' your favorite dessert is ' + favDessert);
-
-
-// //5rd question
-// let favAnimal = prompt('What is your favorite animal?');
-// console.log('userName +  is', favAnimal);
-// //respond
-//alert(username + 'your favorite animal is' + favAnimal);
+alert(userName + 'your favorite animal is' + favAnimal);
 
 
 
 //6th question guess the number I am thinking
 
-// let guessNum = 7;
-// let chances = 4;
+let guessNum = 7;
+let chances = 4;
 
-// while(chances > 0){
-//   chances = chances - 1;
-//   //prompt user for a number
-//   let userResponse = prompt('How many years did I serve in the MN National Guard?');
-//   console.log('userResponse is a string ', typeof(userResponse));
+while(chances > 0){
+  chances = chances - 1;
+  //prompt user for a number
+  let userResponse = prompt('How many years did I serve in the MN National Guard?');
+  console.log('userResponse is a string ', typeof(userResponse));
 
-//   let userNumber =  Number(userResponse);
-//   console.log('userResponse is a num ', userNumber, typeof(userNumber));
+  let userNumber =  Number(userResponse);
+  console.log('userResponse is a num ', userNumber, typeof(userNumber));
 
-//   if (!userResponse){
-//     break; //incase no input
-//   }
+  if (!userResponse){
+    break; //incase no input
+  }
 
-//   //user answer is correct handle
-//   if(userNumber === guessNum){
-//     alert(userName + ' Well done!');
-//     chances = 0;
-//   }
+  //user answer is correct handle
+  if(userNumber === guessNum){
+    alert(userName + ' Well done!');
+    chances = 0;
+  }
 
 
-//   //handle too high or too low
-//   if(userNumber < guessNum){
-//     alert(userName + 'you are too low, keep trying!');
-//   }
+  //handle too high or too low
+  if(userNumber < guessNum){
+    alert(userName + 'you are too low, keep trying!');
+  }
 
-//   //too high if
-// }//closes while
-// alert('the correct answer is ' + guessNum);
+  //too high if
+}//closes while
+alert('the correct answer is ' + guessNum);
 
 
 
 //create an array
 
-let states = ['ILLINOIS','IOWA','ARKANSAS','CALIFORNIA','TEXAS','OMAN','GERMANY'];
-console.log(states.length);
-
-let attempts = 0;
-let correctAnswer = 0;
-
-while(attempts !== 7){
-  let userAnswer = prompt('What states of the world have I lived in?');
-  let userAnswerTrimmedToUpperCase = userAnswer.toUpperCase().trim();
-  console.log('userAnswerTrimmedToUpperCase',userAnswerTrimmedToUpperCase);
-
-  if(states.indexOf(userAnswerTrimmedToUpperCase) >= 0){
-    alert('Yes! ' + userAnswerTrimmedToUpperCase + ' is one of my states of residence.');
-    correctAnswer = correctAnswer + 1;
-    // correctAnswer += 1;
-    // correctAnswer++;
-  } else {
-    alert('Your wrong@!!#%^#%%^');
-  }
-
-<<<<<<< HEAD
-  //too high if
-}//closes while
-alert('the correct answer is ' + guessNum);
 let states = ['poke bowl','salmon chazuke','rice','brats','noodles','sushi','coffee'];
 console.log(states.length);
 
@@ -139,11 +115,6 @@ while(attempts !== 7){
 alert('All of my favorite foods are: ' + states.join(', '));
 alert('You answered ' + correctAnswer + ' out of 7 correctly');
 alert('Thanks for Playin...');
-=======
-  attempts = attempts + 1;
-
-}
->>>>>>> 395a5a9040f364e01a72a00fce1bcddfcd1d0649
 
 alert('All states that I lived in are: ' + states.join(', '));
 alert('You answered ' + correctAnswer + ' out of 7 correctly');
